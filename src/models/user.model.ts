d782a2550a23d0d15b7a485ad429377d4995f7c8
Vitalii Demchyshyn <demchyshyn.v87@gmail.com>
2025-03-17 20:59:03 +0100

    lesson4

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,13 +1,13 @@
-import {Schema, model} from "mongoose";
-import {IUser} from "../interfaces/user.interface";
+import { Schema, model } from "mongoose";
+import { IUser } from "../interfaces/user.interface";
 
 const userSchema = new Schema(
     {
-        name:{type:String, required:true},
-        surname:{type:String, required:true},
-        age:{type:Number, required:true}
+        name: { type: String, required: true },
+        surname: { type: String, required: true },
+        age: { type: Number, required: true },
     },
-    {timestamps:true, versionKey:false}
+    { timestamps: true, versionKey: false },
 );
 
-export const User = model<IUser>("user", userSchema)
+export const User = model<IUser>("user", userSchema);
